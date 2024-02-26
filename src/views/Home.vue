63fc48b82eb1633d48ba274798b15d2adf35ea7b
cailang <3299903308@qq.com>
2024-02-26 21:02:11 +0800

    个人信息维护和密码修改功能完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,7 +16,8 @@
                                                                        alt="avatar"/>-->
                       {{ sysUserName }}</span>
                     <el-dropdown-menu slot="dropdown">
-                        <el-dropdown-item>个人信息</el-dropdown-item>
+                        <el-dropdown-item  @click.native="showPersonalInfo">个人信息</el-dropdown-item>
+                      <el-dropdown-item  @click.native="updatePwd">修改密码</el-dropdown-item>
 <!--                        <el-dropdown-item>设置</el-dropdown-item>-->
                         <el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
                     </el-dropdown-menu>
@@ -84,6 +85,47 @@
                 </div>
             </section>
         </el-col>
+      <!--新增界面-->
+      <el-dialog title="个人信息" :visible.sync="saveFormVisible" :close-on-click-modal="false">
+        <el-form :model="saveForm" label-width="80px" :rules="saveFormRulesPersonInfo" ref="saveFormPersonInfo">
+          <el-form-item label="用户名" prop="username" :error="usernameError">
+            <el-input v-model="saveForm.username" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input show-password type="password" v-model="saveForm.password" auto-complete="off" disabled></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email" :error="emailError">
+            <el-input type="email" v-model="saveForm.email" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="年龄">
+            <el-input-number v-model="saveForm.age" :min="0" :max="200"></el-input-number>
+          </el-form-item>
+          <el-form-item label="部门名称">
+            <el-input v-model="saveForm.department.name" disabled></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click.native="cancelSaveSubmit">取消</el-button>
+          <el-button type="primary" @click.native="saveSubmit" :loading="saveLoading">提交</el-button>
+        </div>
+      </el-dialog>
+      <el-dialog title="修改密码" :visible.sync="updatePwdShow">
+        <el-form :model="updPwdObj" ref="updPwdForm">
+          <el-form-item label="旧密码" prop="oldPassword">
+            <el-input type="password" v-model="updPwdObj.oldPassword" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="新密码" prop="newPassword">
+            <el-input type="password" v-model="updPwdObj.newPassword" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码" prop="confirmPassword">
+            <el-input type="password" v-model="updPwdObj.confirmPassword" autocomplete="off"></el-input>
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="submitUpdatePassword">确 定</el-button>
+        </div>
+      </el-dialog>
     </el-row>
 </template>
 
@@ -94,10 +136,24 @@ import {createPermission} from "../main";
 export default {
     data() {
         return {
+          usernameError: '',
+          emailError: '',
+          saveFormRulesPersonInfo:[
+            {
+              username: [
+                {required: true, message: '请输入用户名', trigger: ['blur', 'change']}
+              ],
+              email: [
+                { required: true, message: '请输入邮箱地址', trigger: ['blur', 'change'] },
+                { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] }
+              ]
+            }
+          ],
             sysName: '物资设备管理系统',
             collapsed: false,
             sysUserName: '',
             sysUserAvatar: '',
+          saveLoading:false,
             form: {
                 name: '',
                 region: '',
@@ -108,10 +164,181 @@ export default {
                 resource: '',
                 desc: ''
             },
-            menuTree: []
-        }
+            menuTree: [],
+          saveFormVisible:false,
+          saveForm:{
+            age: 34,
+            departmentId: 1,
+            department:{
+              name: '',
+            },
+            email:"",
+            id:null,
+            password:"",
+            username:""
+        },
+          updatePwdShow:false,
+          updPwdObj: {
+            oldPassword: '',
+            newPassword: '',
+            confirmPassword: ''
+          }
+      }
     },
     methods: {
+      updatePwd(){
+        this.updatePwdShow = true;
+      },
+      submitUpdatePassword() {
+        if (!this.updPwdObj.oldPassword) {
+          this.$message.error('请输入旧密码');
+          return;
+        }
+        if (!this.updPwdObj.newPassword || this.updPwdObj.newPassword.length < 6) {
+          this.$message.error('新密码长度不能少于6位');
+          return;
+        }
+        if (this.updPwdObj.newPassword !== this.updPwdObj.confirmPassword) {
+          this.$message.error('两次输入的新密码不一致');
+          return;
+        }
+        // 如果所有校验通过，则进行密码更新操作
+        // ... 这里调用更新密码的API
+        var user = localStorage.getItem('loginUser');
+        if (user) {
+          user = JSON.parse(user);
+          if (user.id!=null&&user.id!="") {
+            this.$http.get("/employee/"+user.id).then((data) => {
+              this.saveLoading = false;
+              console.log(data)
+              this.saveForm = data.data.resultObject
+              if (this.saveForm.password != this.updPwdObj.oldPassword){
+                //说明旧密码验证错误
+                this.$message({
+                  message: '旧密码错误！',
+                  type: 'error'
+                });
+                return
+              }
+              this.saveForm.password=this.updPwdObj.newPassword;
+              this.$http.put("/employee", this.saveForm).then((data) => {
+                this.saveLoading = false;
+                data = data.data
+                if (data.success) {
+                  this.$message.success('密码修改成功');
+                  this.updatePwdShow = false;
+                  // 可能需要重置表单
+                  this.resetForm();
+                } else {
+                  this.$message({
+                    message: '操作失败',
+                    type: 'error'
+                  });
+                }
+              });
+            }).catch(data => {
+              this.$message({
+                message: '网络错误！',
+                type: 'error'
+              });
+            })
+          }else{
+            this.$message({
+              message: '登录失效，请重新登录！',
+              type: 'error'
+            });
+            this.logout();
+          }
+        }
+
+      },
+      resetForm() {
+        this.$refs.updPwdForm.resetFields();
+        this.updPwdObj = {
+          oldPassword: '',
+          newPassword: '',
+          confirmPassword: ''
+        };
+      },
+      validateEmail(email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+      },
+      showPersonalInfo(){
+        var user = localStorage.getItem('loginUser');
+        if (user) {
+          user = JSON.parse(user);
+          if (user.id!=null&&user.id!="") {
+            this.$http.get("/employee/"+user.id).then((data) => {
+              this.saveLoading = false;
+              console.log(data)
+              this.saveForm = data.data.resultObject
+              this.saveFormVisible = true
+            }).catch(data => {
+                  this.$message({
+                    message: '网络错误！',
+                    type: 'error'
+                  });
+                })
+          }else{
+            this.$message({
+              message: '登录失效，请重新登录！',
+              type: 'error'
+            });
+            this.logout();
+          }
+        }
+      },
+      cancelSaveSubmit() {
+        this.saveFormVisible = false
+      },
+      //新增
+      saveSubmit: function () {
+        //对表单数据进行校验
+        this.$refs.saveFormPersonInfo.validate((valid) => {
+          if (valid) {
+            this.usernameError = '';
+            this.emailError = '';
+
+            // Validate username
+            if (!this.saveForm.username) {
+              this.usernameError = '请输入用户名';
+            } else if (this.saveForm.username.length < 3 || this.saveForm.username.length > 15) {
+              this.usernameError = '用户名长度在 3 到 15 个字符';
+            }
+
+            // Validate email
+            if (!this.saveForm.email) {
+              this.emailError = '请输入邮箱地址';
+            } else if (!this.validateEmail(this.saveForm.email)) {
+              this.emailError = '请输入正确的邮箱地址';
+            }
+
+            if (this.usernameError || this.emailError) {
+              return false; // If there are errors, return and do not submit.
+            }
+            this.$confirm('确认提交吗？', '提示', {}).then(() => {
+              this.saveLoading = true;
+              this.$http.put("/employee", this.saveForm).then((data) => {
+                this.saveLoading = false;
+                data = data.data
+                if (data.success) {
+                  this.$message({
+                    message: '操作成功',
+                    type: 'success'
+                  });
+                  this.saveFormVisible = false;
+                } else {
+                  this.$message({
+                    message: '操作失败',
+                    type: 'error'
+                  });
+                }
+              });
+            });
+          }
+        });
+      },
         getMenuTree(employeeId) {
             this.$http.get("/menu/getMenuTree/" + employeeId)
                 .then(data => {
