5624ba8a967d15f12652fcbda0af14c385998989
cailang <3299903308@qq.com>
2024-03-11 21:51:21 +0800

    knife4j接口文档展示

--- src/views/sys/InterfaceDoc.vue
+++ src/views/sys/InterfaceDoc.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
-    <h2>Swagger接口文档信息</h2>
+    <h2>后端接口文档信息</h2>
     <iframe ref="swaggerIframe" :src="iframeUrl" style="width: 100%; height: 600px;"></iframe>
+<!--    <div v-html="interfaceContent"></div>-->
   </div>
 </template>
 
@@ -10,26 +11,18 @@ export default {
   name: "InterfaceDoc",
   data () {
     return {
-      iframeUrl:""
+      interfaceContent:"",
+      iframeUrl:"http://localhost:8080//doc.html"
     }
   },
   mounted() {
-    const token = localStorage.getItem('token'); // 从本地存储中获取token
-    const loginUser = localStorage.getItem('loginUser'); // 从本地存储中获取loginUser
-    // http://localhost:8080/swagger-ui/
-    const swaggerUrl = 'http://localhost:8080/swagger-ui.html';
-
-    // 拼接URL参数
-    const params = new URLSearchParams();
-    params.append('token', token);
-    params.append('loginUser', loginUser);
-
-    this.iframeUrl = `${swaggerUrl}?${params}`;
-    // 监听iframe加载完成事件
-    this.$refs.swaggerIframe.addEventListener('load', () => {
-      // 在iframe加载完成后可以进行一些操作，比如向iframe发送消息
-      this.$refs.swaggerIframe.contentWindow.postMessage({ token, loginUser }, '*');
-    });
+    // this.$http.get("/doc.html")
+    //     .then(data => {
+    //       this.interfaceContent = data.data
+    //     })
+    //     .catch(error => {
+    //       console.log(error)
+    //     })
   }
 }
 </script>
